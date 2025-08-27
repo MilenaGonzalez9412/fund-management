6e6c58cc6c1fbac6c3474da39a57a819992335c5
Milena Gonzalez <milecodes@gmail.com>
2025-08-27 10:05:37 -0500

    Add intl package to format data,

--- lib/config/theme/extensions.dart
+++ lib/config/theme/extensions.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 extension ThemeContext on BuildContext {
   ThemeData get theme => Theme.of(this);
   ColorScheme get colorScheme => theme.colorScheme;
+}
+
+String formatDate(String fullDate) {
+  final date = DateTime.parse(fullDate);
+  return DateFormat('yyyy-MM-dd').format(date);
+}
+
+String formatCurrency(double value) {
+  final formatter = NumberFormat('#,##0', 'es_CO');
+  return '\$${formatter.format(value)} COP';
 }
